4abad16e583cccbb88b98c4b6c8458625b307c0f
Jonathan Bergerot <jobergerot@gmail.com>
2023-02-27 17:37:52 +0100

    add new/create at places_controller and routes update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,9 +8,26 @@ class PlacesController < ApplicationController
   def show
   end
 
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to place_path(@place)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_place
     @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :capacity, :price_per_day, :address, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
     resources :places
   end
   devise_for :users
+  resources :places, only: %i[index show new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: "places#index"
-  get 'places/:id', to: 'places#show', as: 'place'
 end
