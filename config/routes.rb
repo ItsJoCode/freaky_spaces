fc6d17bae10c0c9a98688d094c1e761df669d5ec
Jonathan Bergerot <jobergerot@gmail.com>
2023-02-27 17:17:03 +0100

    create owner namespaces with owner/places_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  namespace :owner do
+    resources :places
+  end
   devise_for :users
-  root to: "places#index"
-  get 'places/:id', to: 'places#show', as: 'place'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "places#index"
+  get 'places/:id', to: 'places#show', as: 'place'
 end
