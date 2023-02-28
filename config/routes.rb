506a2d63d73f0617ae868581a61959b097a19979
Jonathan Bergerot <jobergerot@gmail.com>
2023-02-28 15:17:50 +0100

    add MVC owner

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :owner do
     resources :places
-    resources :bookings
+    resources :bookings, only: %i[index show]
   end
   resources :places, only: %i[index show new create] do
     resources :bookings, only: %i[new create]
