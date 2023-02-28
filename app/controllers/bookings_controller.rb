3f801f9f74bb8661bd39dc1516f5b3a6e04452cf
RL <ronan.laporte@gmail.com>
2023-02-28 10:47:25 +0100

    modification controller booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,26 @@
 class BookingsController < ApplicationController
-
-  before_action :set_booking, only: [:show]
+  before_action :set_booking, only: [:show, :destroy]
 
   def index
-    @bookings = booking.all
+    @user = User.find(params(:user_id))
   end
 
   def show
+    @user = User.find(params(:user_id))
   end
 
   def new
+    @user = User.find(params(:user_id))
+    @place = Place.find(params(:place_id))
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @user = User.find(params(:user_id))
+    @place = Place.find(params(:place_id))
+    @booking.user = @user
+    @booking.place = @place
     if @booking.save
       redirect_to booking_path(@booking)
     else
@@ -22,15 +28,18 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:end_date, :start_date, :place_id, :user_id)
+    params.require(:booking).permit(:end_date, :start_date)
   end
 
   def set_booking
     @booking = Booking.find(params[:id])
   end
-
 end
